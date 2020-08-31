ae4f74fc1790d941aac3b87e84f432f90dc46e8d
lsj <lsj@DESKTOP-RO130SO>
2020-08-31 16:31:38 +0900

    샘플 구현

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -3,21 +3,19 @@ using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
-	public class HelloWorldController : Controller
-	{
-		// 
-		// GET: /HelloWorld/
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
-		// 
-		// GET: /HelloWorld/Welcome/ 
-		public string Welcome(string name, int numTimes = 1)
-		{
-			return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-		}
-	}
+            return View();
+        }
+    }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MvcMovie
 {
@@ -24,6 +27,18 @@ namespace MvcMovie
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+
+			services.AddDbContext<MvcMovieContext>(options =>
+					options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
+
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+			services.ConfigureApplicationCookie(options =>
+			{
+				options.ExpireTimeSpan = TimeSpan.FromHours(1);
+				options.LoginPath = "/Account/LogIn";
+				options.LogoutPath = "/Account/LogOut";
+				options.AccessDeniedPath = "/Home/Index";
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
